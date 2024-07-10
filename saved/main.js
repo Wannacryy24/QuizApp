734d64e40ba677474794b408042cd99c096117c5
Mayank Verma <muftimayank@gmail.com>
2024-07-10 12:34:27 +0530

    added routing

--- saved/main.js
+++
@@ -1,9 +0,0 @@
-import './style.css';
-import { frontHeader } from "./frontPage.js";
-import { router } from './router.js';
-
-document.addEventListener('DOMContentLoaded', function () {
-    console.log("DOM fully loaded");
-    frontHeader();
-    router();
-});
